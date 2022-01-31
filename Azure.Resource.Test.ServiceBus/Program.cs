1cd6a665070f44bf56540b17942f9b29f8198734
johnlope <johnlope@gmail.com>
2022-01-31 15:12:51 -0500

    Add project files.

---
+++ Azure.Resource.Test.ServiceBus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure.Resource.Test.ServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
